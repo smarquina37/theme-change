1dad25eed7a03c5f6c72b8750ab1e4381e52e3bd
Susana Marquina <smarq86@gmail.com>
2021-12-03 19:26:12 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const body = document.getElementById('body');
+const greeting = document.getElementById('greeting');
+const christmasBtn = document.getElementById('christmas');
+const snowBtn = document.getElementById('snow');
+
+// Task:
+//- Add the functionality to switch the theme between 'Christmas' and 'snow'.
+
