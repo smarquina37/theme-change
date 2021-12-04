da85a52c03d1a2ec0d50eb3ee06eea6fe152dbc4
Susana Marquina <smarq86@gmail.com>
2021-12-03 19:26:39 -0800

    JS added to toggle theme

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ const snowBtn = document.getElementById('snow');
 
 // Task:
 //- Add the functionality to switch the theme between 'Christmas' and 'snow'.
+function ChristmasTheme() {
+  christmasBtn.addEventListener('click', function () {
+    body.className = 'christmas';
+    greeting.innerHTML = 'Merry Christmas 🎅';
+  });
+}
+ChristmasTheme();
 
+function snowTheme() {
+  snowBtn.addEventListener('click', function () {
+    body.className = 'snow';
+    greeting.innerHTML = 'Frosty the Snowman ☃️';
+  });
+}
+snowTheme();
